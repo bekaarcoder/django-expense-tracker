97d69ecb9926c69c5a2cb644ef23c8e0abe55690
Shashank <s_shashank@outlook.com>
2021-02-22 22:08:56 +0530

    adding and listing expenses

--- expenses/views.py
+++ expenses/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Category
+from .models import Category, Expense
 
 
 @login_required(login_url='/auth/login')
 def index(request):
-  return render(request, 'expenses/index.html')
+  expenses = Expense.objects.filter(owner=request.user).order_by('-date')
+  context = {
+    'expenses': expenses
+  }
+  return render(request, 'expenses/index.html', context)
 
+
+@login_required(login_url='/auth/login')
 def add_expense(request):
   categories = Category.objects.all()
   context = {
@@ -26,9 +32,21 @@ def add_expense(request):
 
     if not amount:
       messages.error(request, 'Amount is required.')
+      return render(request, 'expenses/add_expense.html', context)
     if not description:
       messages.error(request, 'Description is required.')
+      return render(request, 'expenses/add_expense.html', context)
     if not category:
       messages.error(request, 'Category is required.')
+      return render(request, 'expenses/add_expense.html', context)
 
-    return render(request, 'expenses/add_expense.html', context)
+    Expense.objects.create(
+      owner=request.user,
+      amount=amount,
+      date=date,
+      category=category,
+      description=description
+    )
+    messages.success(request, 'Expense saved successfully.')
+
+    return render(request, 'expenses/add_expense.html')
