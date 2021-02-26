a8f81e0d31562f47f2bdc339a8a136ad19a55341
Shashank <s_shashank@outlook.com>
2021-02-26 21:42:20 +0530

    edit and delete income

--- income/urls.py
+++ income/urls.py
@@ -5,5 +5,7 @@ app_name = 'income'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add-income', views.add_income, name='add-income')
+    path('add-income', views.add_income, name='add-income'),
+    path('edit-income/<int:id>', views.edit_income, name='edit-income'),
+    path('delete-income/<int:id>', views.delete_income, name='delete-income')
 ]
--- income/views.py
+++ income/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Source, Income
 from django.contrib import messages
 from django.core.paginator import Paginator
@@ -60,3 +60,57 @@ def add_income(request):
         messages.success(request, "Income added successfully.")
 
         return render(request, 'income/add_income.html')
+
+
+def edit_income(request, id):
+    income = Income.objects.get(pk=id)
+    sources = Source.objects.all()
+    context = {
+        'income': income,
+        'sources': sources
+    }
+
+    if request.method == 'GET':
+        return render(request, 'income/edit_income.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+        description = request.POST['description']
+        source = request.POST['source']
+        date = request.POST['date']
+
+        if not amount:
+            messages.error(request, "Amount is required.")
+            return render(request, 'income/add_income.html', context)
+        if not description:
+            messages.error(request, "Description is required.")
+            return render(request, 'income/add_income.html', context)
+        if not source:
+            messages.error(request, "Source is required.")
+            return render(request, 'income/add_income.html', context)
+        if not date:
+            date = datetime.date.today()
+
+        income.owner = request.user
+        income.amount = amount
+        income.date = date
+        income.source = source
+        income.description = description
+        income.save()
+
+        messages.success(request, "Income updated successfully.")
+        return redirect('income:index')
+
+
+def delete_income(request, id):
+    income = Income.objects.get(pk=id)
+    context = {
+        'income': income
+    }
+    
+    if request.method == 'POST':
+        income.delete()
+        messages.success(request, 'Income deleted successfully.')
+        return redirect('income:index')
+
+    return render(request, 'income/delete_income.html', context)
